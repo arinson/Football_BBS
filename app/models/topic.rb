b62e3d567898181a6fb03a8fbd996e5499d9d542
arinson <ari.nagashi0308@gmail.com>
2020-10-27 16:43:48 +0900

    Topicモデルvalidates追加

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,2 +1,6 @@
 class Topic < ApplicationRecord
+  validates :user_id, presence: true
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :image, presence: true
 end
