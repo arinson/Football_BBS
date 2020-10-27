78c3f429f2edff342d86697b985b93bf79fe23f2
arinson <ari.nagashi0308@gmail.com>
2020-10-27 17:23:13 +0900

    topicsコントローラ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  get "topics/new"
   devise_for :users
   root "pages#index"
   get "pages/show"
+
+  resources :topics
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
