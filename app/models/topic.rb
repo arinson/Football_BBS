ea69bf71f56b1faa7d3c520b4e336f155c27df47
arinson <ari.nagashi0308@gmail.com>
2020-10-27 17:10:23 +0900

    画像投稿に伴い、設定ファイル修正

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,8 +1,10 @@
 class Topic < ApplicationRecord
+  belongs_to :user
+
   validates :user_id, presence: true
   validates :title, presence: true
   validates :description, presence: true
   validates :image, presence: true
 
-  belongs_to :user
+  mount_uploader :image, ImageUploader
 end
