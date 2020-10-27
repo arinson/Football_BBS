3ca48b714b94cbad39158448b9d5c84470f8cb1e
arinson <ari.nagashi0308@gmail.com>
2020-10-27 18:40:41 +0900

    新規投稿ページ修正

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,4 +1,21 @@
 class TopicsController < ApplicationController
   def new
+    @topic = Topic.new
+  end
+
+  def create
+    @topic = current_user.topics.new(topic_params)
+
+    if @topic.save
+      redirect_to topics_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def topic_params
+    params.require(:topic).permit(:title, :image, :description)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get "topics/new"
   devise_for :users
   root "pages#index"
   get "pages/show"
 
+  get "topics/new"
   resources :topics
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
