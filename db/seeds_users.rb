f9ffd241712822c6b84ce9e95f9f7be1a54e4f3a
arinson <ari.nagashi0308@gmail.com>
2020-11-11 01:14:56 +0900

    seed修正

--- db/seeds_users.rb
+++ db/seeds_users.rb
@@ -1,1 +1,6 @@
-
+User.create!(username: "ゲストユーザー",
+             email: 'test@example.com',
+             password: '123456',
+             password_confirmation: '123456',
+             image: open("#{Rails.root}/app/assets/images/8.png"),
+             admin: false)
