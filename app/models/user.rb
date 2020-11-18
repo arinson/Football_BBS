35c6290b2fd97e04ce1ef22bd88eb66df054ef6f
arinson <ari.nagashi0308@gmail.com>
2020-11-18 21:35:25 +0900

    アソシ修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
   has_many :topics, dependent: :destroy
+  has_many :active_relationships, class_name: "Relationship",
+                                  foreign_key: "follower_id",
+                                  dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
   has_many :likes, dependent: :destroy
   has_many :like_topics, through: :likes, source: "topic"
   has_many :comments, dependent: :destroy
